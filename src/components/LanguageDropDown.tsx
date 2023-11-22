85b67e0a8baeaed8c7b47b4b2aec8e9aa29b85e5
Beksultan <beksultan@Noutbuk-Musan.local>
2023-11-22 11:11:09 +0600

    fix(lang-dropdown): fix

--- src/components/LanguageDropDown.tsx
+++ src/components/LanguageDropDown.tsx
@@ -1,29 +1,25 @@
 import { useState } from "react"
 import { languages } from '../i18n/ui';
-import { getLangFromUrl, useTranslatedPath } from "../i18n/utils";
+import { getLangFromUrl, useTranslatedPath, useTranslations } from "../i18n/utils";
 
 const LanguageDropDown = () => {
     const [open, setOpen] = useState(false)
-    const [currentLang, setCurrentLang] = useState('РУС')
 
     const lang = getLangFromUrl(new URL(window.location.href))
     const translatePath = useTranslatedPath(lang);
+    const t = useTranslations(lang)
 
     const toggle = (() => setOpen(!open))
-    const handleLangChange = (lang: string) => {
-        setCurrentLang(lang)
-        window.location.href = translatePath('/', lang)
-    }
 
     return (
         <div className="relative" onClick={toggle}>
             <div className="flex gap-1 cursor-pointer">
                 <img src="/arrow-down.svg" alt="arrow"/>
-                <span className="text-main">{currentLang}</span>
+                <span className="text-main">{languages[lang]}</span>
             </div>
             {open && <div className="absolute top-8 right-0 z-20 bg-white shadow py-4 px-5 rounded-xl text-main flex flex-col gap-4">
             {Object.entries(languages).map(([lang, label]) => (
-                <span key={label} onClick={() => handleLangChange(lang)} className="cursor-pointer uppercase">{label}</span>
+                <a key={label} href={translatePath('/', lang)} className="cursor-pointer uppercase">{label}</a>
             ))}
             </div>}
         </div>
